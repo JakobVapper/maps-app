44e96e07c095c1c40ddfbfe1755a1dace9383a24
JakobVapper <jakob.vapper@ametikool.ee>
2025-04-01 09:44:37 +0300

    Installed Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MarkerController;
-use App\Http\Controllers\WeatherController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/markers', [MarkerController::class, 'index'])->name('markers.index');
-Route::post('/markers', [MarkerController::class, 'store'])->name('markers.store');
-Route::get('/markers/all', [MarkerController::class, 'show'])->name('markers.show');
-Route::put('/markers/{marker}', [MarkerController::class, 'update'])->name('markers.update');
-Route::post('/markers/{marker}', [MarkerController::class, 'update']);
-Route::delete('/markers/{marker}', [MarkerController::class, 'destroy'])->name('markers.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/weather/{city}', [WeatherController::class, 'getWeather']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
